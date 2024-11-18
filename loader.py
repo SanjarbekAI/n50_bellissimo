01b7ca5d1f271aab49fa306e701f271fec27177d
SanjarbekAI <sanjarbekwork@gmail.com>
2024-11-18 17:42:06 +0500

    main menu added

--- loader.py
+++ loader.py
@@ -10,7 +10,7 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARNING,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler(),
