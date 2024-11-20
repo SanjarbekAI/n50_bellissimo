20920847d46a7de31062dcca9129116d69f40e01
SanjarbekAI <sanjarbekwork@gmail.com>
2024-11-20 17:55:28 +0500

    changed

--- utils/get_location.py
+++ utils/get_location.py
@@ -1,30 +1,6 @@
-from geopy.geocoders import Nominatim
-import requests
 import asyncio
 
-
-def get_location_name(latitude, longitude):
-    # Initialize Nominatim API
-    geolocator = Nominatim(user_agent="geoapiExercises")
-
-    # Get location
-    location = geolocator.reverse((latitude, longitude), exactly_one=True)
-
-    # Extract address details
-    address = location.raw['address']
-    city = address.get('city', '')
-    state = address.get('state', '')
-    country = address.get('country', '')
-    street = address.get('road', '')
-    postcode = address.get('postcode', '')
-
-    return {
-        'city': city,
-        'state': state,
-        'country': country,
-        'street': street,
-        'postcode': postcode
-    }
+import requests
 
 
 async def get_full_address(longitude, latitude):
@@ -33,6 +9,5 @@ async def get_full_address(longitude, latitude):
     url = f"https://nominatim.openstreetmap.org/reverse?format=json&lat={latitude}&lon={longitude}"
     response = requests.get(url)
     if response.status_code == 200:
-
         return response.json()['display_name']
     return False
